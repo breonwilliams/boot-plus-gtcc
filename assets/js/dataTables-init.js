138d25d575b7895ce133d7c2441f9417c915b0c9
breonw <breon_williams@unc.edu>
2016-10-27 20:30:56 -0400

    modify datatables

--- assets/js/dataTables-init.js
+++ assets/js/dataTables-init.js
@@ -1,7 +1,7 @@
 jQuery(function ($) {
   //Only needed for the filename of export files.
   //Normally set in the title tag of your page.
-  document.title='Simple DataTable';
+  //document.title='Simple DataTable';
   // DataTable initialisation
   $('#coursesTable').DataTable(
       {
