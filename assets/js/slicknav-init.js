8d777979d2697acb1b493d72024e2f65f3234344
breonw <breon_williams@unc.edu>
2016-11-05 18:56:08 -0400

    modify styles

--- assets/js/slicknav-init.js
+++ assets/js/slicknav-init.js
@@ -1,12 +1,15 @@
 jQuery(function ($) {
-$('#gtcc-slick').slicknav({
-    label: 'GTCC Menu',
-    duration: 500,
-    prependTo:'#gtcc-menu',
-    init: function( autoopen ){}
-});
+    if($('#gtcc-menu').length) {
+        $('#gtcc-slick').slicknav({
+            label: 'GTCC Menu',
+            duration: 500,
+            prependTo: '#gtcc-menu',
+            init: function (autoopen) {
+            }
+        });
 
-$(function( autoopen ) {
-    jQuery('#gtcc-slick').slicknav('close');
-});
+        $(function (autoopen) {
+            jQuery('#gtcc-slick').slicknav('close');
+        });
+    }
 });
