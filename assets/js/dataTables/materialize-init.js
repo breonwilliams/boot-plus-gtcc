657b83bbd815ab2080b99e76ade5b7dc01128f46
breonw <breon_williams@unc.edu>
2017-05-23 12:40:05 -0400

    modify datatable js

--- assets/js/dataTables/materialize-init.js
+++ assets/js/dataTables/materialize-init.js
@@ -1,35 +1,37 @@
-jQuery(function ($) {
-    var table = $('#coursesTable').DataTable({
+var table;
+jQuery(function($) {
+    table = $('#coursesTable').DataTable({
         dom: 'lrtip',
-        initComplete: function () {
-            this.api().columns([2]).every( function () {
+        initComplete: function() {
+            this.api().columns([2]).every(function() {
                 var column = this;
                 console.log(column);
                 var select = $("#instructorFltr");
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' )
-                } );
-            } );
-            this.api().columns([3]).every( function () {
+                column.data().unique().sort().each(function(d, j) {
+                    select.append('<option value="' + d + '">' + d + '</option>')
+                });
+            });
+            this.api().columns([3]).every(function() {
                 var column = this;
                 console.log(column);
                 var select = $("#courseNumberFltr");
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' )
-                } );
-            } );
+                column.data().unique().sort().each(function(d, j) {
+                    select.append('<option value="' + d + '">' + d + '</option>')
+                });
+            });
             $("#instructorFltr,#courseNumberFltr").material_select();
 
-            $('.dataTables_filter').each(function () {
+            $('.dataTables_filter').each(function() {
                 $(this).append('<button id="test" class="btn btn-default" type="button">Clear Search</button>');
             });
 
             var select = $('#dataTablesSelect select');
-            $("#test").click(function(){
-                table.search('').draw();
-                //$("form input").val("");
-                //$("#dataTablesSelect select").val('').change();
-                //select.material_select();        //Update material select
+            $("#test").click(function() {
+
+                select.val(""); //Sets the first option as selected
+                select.material_select();
+                select.trigger("change");
+                table.search('').columns().search('').draw();
             });
 
         },
@@ -43,26 +45,37 @@ jQuery(function ($) {
             'print',
         ]
     });
-
-    $('#instructorFltr').on('change', function(){
-        var search = [];
-
-        $.each($('#instructorFltr option:selected'), function(){
-            search.push($(this).val());
-        });
-
-        search = search.join('|');
-        table.column(2).search(search, true, false).draw();
-    });
-
-    $('#courseNumberFltr').on('change', function(){
-        var search = [];
-
-        $.each($('#courseNumberFltr option:selected'), function(){
-            search.push($(this).val());
-        });
-
-        search = search.join('|');
-        table.column(3).search(search, true, false).draw();
-    });
 });
+jQuery(document).ready(function($){
+$('#instructorFltr').on('change', function() {
+    var search = [];
+
+    $.each($('#instructorFltr option:selected'), function() {
+        search.push($(this).val());
+    });
+    if(search.length)
+    {
+        if(search[0]=="")
+            search.splice(0,1);
+    }
+    search = search.join('|');
+    table.column(2).search(search, true, false).draw();
+});
+
+$('#courseNumberFltr').on('change', function() {
+    var search = [];
+
+    $.each($('#courseNumberFltr option:selected'), function() {
+        search.push($(this).val());
+    });
+    if(search.length)
+    {
+        if(search[0]=="")
+            search.splice(0,1);
+    }
+    search = search.join('|');
+    table.column(3).search(search, true, false).draw();
+    console.log("search"+search);
+});
+});
+
